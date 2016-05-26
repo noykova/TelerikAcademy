e0be468430123ae1aebcac61f0891fd8bd577a54
Neli Noykova <noykova@users.noreply.github.com>
2016-05-27 01:29:05 +0300

    07.Exception Handling homework added

---
+++ C#AdvancedHomework/7_Exception_Handling/01_Square_root/Square_root.cs
@@ -0,0 +1,57 @@
+﻿/*Write a program that reads an integer number and calculates and prints its square root.
+•	If the number is invalid or negative, print Invalid number.
+•	In all cases finally print Good bye. Use try-catch-finally block.
+Input
+•	On the only line you will receive a real number
+Output
+•	Print the square root of the number or Invalid number on the first line 
+o	Use 3 digits of precision
+•	Print Good bye on the second line
+ * 
+ * BGCoder - 100/100
+ * 
+*/
+
+using System;
+
+    class Square_root
+    {
+        static void Main()
+        {
+            try
+            {
+                //Console.Write("Enter an integer number a = ");
+                string str = Console.ReadLine();
+                double a = double.Parse(str);
+                if (a < 0 || a > uint.MaxValue)
+                {
+                    throw new ArgumentOutOfRangeException("a");
+               }
+
+                double result = Math.Sqrt(a);
+                Console.WriteLine(String.Format("{0:F3}", Math.Round(result,3)));
+            }
+
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid number"); //+ fe.Message);
+            }
+
+            catch (ArgumentOutOfRangeException ae)
+            {
+                Console.WriteLine("Invalid number");
+            } //out of range [" + 0 + "..." + int.MaxValue + "]. " + ae.Message); }
+
+                //catch (OverflowException oe)
+                //{
+                //    Console.WriteLine(
+                //        "Invalid number!\nThe number is too big to fit in Int32! " + oe.Message);
+                //}
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+
+    }
+
---
+++ C#AdvancedHomework/7_Exception_Handling/02_Enter_numbers/Enter_numbers.cs
@@ -0,0 +1,101 @@
+﻿/*Write a method ReadNumber(int start, int end) that enters an integer number in a given range ( start, end ).
+•	If an invalid number or non-number text is entered, the method should throw an exception. 
+ * Using this method write a program that enters 10 numbers: a1, a2, ..., a10, such that 0 < a1 < ... < a10 < 100
+Input
+•	You will receive 10 lines of input, each consisted of an integer number 
+o	a1
+o	a2
+o	...
+o	a10
+Output
+•	Print 0 < a1 < ... < a10 < 100 
+o	Or Exception if the above inequality is not true
+ * 
+ * BGCoder - 80/100
+*/
+
+using System;
+
+public class Enter_numbers
+{
+    //fields
+    private static int number; //return value of the method ReadNumber(int start, int end) 
+
+    //constructors -define which properties are mandatory and which - optional
+    public Enter_numbers()
+    {
+    }
+
+    //methods
+    public static int ReadNumber(int start, int end)
+
+    {
+            try
+            {
+                //Console.Write("Enter an integer number a = ");
+                string str = Console.ReadLine();
+                int a = int.Parse(str);
+                if (a <= start || a >= end)
+                {
+                    throw new ArgumentOutOfRangeException("The number is not in range!");
+                }
+                number = a;
+            }
+
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Exception"); //"Invalid number. " + fe.Message);
+            }
+
+            catch (ArgumentOutOfRangeException ae)
+            {
+                Console.WriteLine("Exception");
+            } //"Invalid number out of range [" + start + "..." + end + "]. " + ae.Message); }
+
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("Exception");
+                //"Invalid number!\nThe number is too big to fit in Int32! " + oe.Message);
+            }
+        return number;
+    }
+
+
+
+    public static void Main()
+    {
+        int start = 0;
+        int end = 100;
+        int indicator = 0; 
+        int[] numbers = new int[10];
+        numbers[0] = ReadNumber(start, end); 
+        //numbers[11] = 100;
+
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+
+            numbers[i] = ReadNumber(start, end);
+
+            if (numbers[i-1]<numbers[i])
+            {
+            indicator++;                
+            }
+
+        }
+
+        if (indicator == 9 && 1 < numbers[0])
+        {
+        Console.WriteLine("1 < "+string.Join(" < ", numbers)+" < 100");      
+        }
+        else if (indicator == 9 && 1 == numbers[0])
+        {
+            Console.WriteLine("0 < " + string.Join(" < ", numbers) + " < 100");     
+        }
+        else
+        {
+            Console.WriteLine("Exception");
+        }
+      
+    }
+}
